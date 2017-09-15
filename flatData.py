32fbc276e5d7ec92ff606ddd5202528c110d3599
unknown <laptopjohn10@gmail.com>
2017-09-15 16:44:05 -0500

    Completed rewrite of DataHandler

--- flatData.py
+++ flatData.py
@@ -11,20 +11,27 @@ class DataHandler:
   def __getitem__(self,key,value):
     try:
       return self.changes[key]
-    except:
+    except KeyError:
       try:
         return self.sources[key]
-      except:
+      except KeyError:
         print("self.name: " + str(key) + " does not exist")
     
-  def __setitem__(self,key):
+  def __setitem__(self,key,value):
     if not (self.source.__contains__(key)):
       self.source[key] = value
       self.changes[key] = value
       return
     if not (self.changes.__contains__(key) and self.changes[key]==value):
       self.changes[key] = value
+    if self.source[key] == self.changes[key]:
+      self.changes.delitem(key)
       
+  def applyChanges(self):
+    for key in self.changes:
+      self.source[key] = self.changes[key]
+      self.changes.delitem[key]
+
   def getUpdate(self):
     result = self.changes
     self.changes = {}
@@ -32,24 +39,47 @@ class DataHandler:
     
   def putUpdate(self,update):
     for key in update:
-      pass
+      if self.changes.__contains__(key):
+        self.changes.delitem(key)
+      self.source[key] = update[key]
+      #self.changes[key] = update[key]
       
+    
   def getRefresh(self):
-    pass
+    result = {}
+    for key in self.source:
+      try:
+        result[key] = self.changes[key]
+      except KeyError:
+        result[key] = self.source[key]
     
-  def putRefresh(self):
-    pass
+  def putRefresh(self,refresh):
+    self.source = refresh
 
-def encodeUpdate(object):
-  pass
-  
-def decodeUpdate(object,input):
-  pass
-  
-def encodeRefresh(object):
-  pass
-  
-def decodeRefresh(object,input):
-  pass
     
-  
+    
+    
+    
+def encodeUpdate(object): #these methods allow recursion where __str__ would break it.
+  try:
+    return object.getUpdate()
+  except AttributeError:
+    return str(object).encode()
+    
+def encodeRefresh(object):
+  try:
+    return object.getRefresh()
+  except AttributeError:
+    return str(object).encode()
+
+def decodeUpdate(object,input):
+  try:
+    object.putUpdate(input)
+  except AttributeError:
+    object = input
+    
+def decodeRefresh(object,input):
+  try:
+    object.putRefresh(input)
+  except AttributeError:
+    object = input
