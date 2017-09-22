4c4a98e72db5901729a52e1a064f561555f4918d
unknown <laptopjohn10@gmail.com>
2017-09-22 11:50:39 -0500

    created ByteHandler, a subclass of DataHandler, to force custom refresh encodings

---
+++ flatByte.py
@@ -0,0 +1,16 @@
+
+
+
+class ByteHandler(DataHandler):
+
+  def __init__(self,source,name="unnIH"):
+    DataHandler.__init__(self,source,name=name)
+    
+  def getRefresh(self):
+    print("getting refresh (direct) for " + self.name)
+    return self.source
+    
+  def putRefresh(self,refresh):
+    print("putting refresh (direct) for " + self.name)
+    self.changes = {}
+    self.source = refresh
